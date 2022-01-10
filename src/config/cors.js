dfe80424d52ba0cd5c42f97fdc9d40eecbe8fa1a
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2022-01-10 15:03:29 -0300

    Testando API(Postman) & Habilitando o CORS

---
+++ src/config/cors.js
@@ -0,0 +1,5 @@
+module.exports = function(req, res, next){
+    res.header('Acess-Control-Allow-Origin', '*')
+    res.header('Acess-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.header('Acess-Control-Allow-Methods', 'Origin, X-Requested-With, Content-Type, Accept')
+}
